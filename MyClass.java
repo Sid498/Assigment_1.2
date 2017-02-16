02781a83e50c509959047bc6d1f9324149a9cb5a
Sid498 <sidpawar619@gmail.com>
2017-02-16 12:41:41 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,18 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		int a=20,b=10;
+		 b = a-- - --a;
+		int c = a--;
+		int d = a >> 2;
+		int e = a & b;
+		System.out.println("a "+a+"\n"+
+				"b "+b+"\n"+
+				"c "+c+"\n"+
+				"d "+d+"\n"+
+				"e "+e+"\n"+
+				"d "+d);
+	}
+
+}
